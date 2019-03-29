184bf4f30b9fb95b878ad814f206ea073b9296b5
Zachary Townsend <zach@verbbrands.com>
2019-03-29 16:33:15 +0100

    Setup dev environment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,40 @@
-import React from "react";
-import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
+import React from 'react';
+import { Link } from 'gatsby';
+import github from '../img/github-icon.svg';
+import logo from '../img/logo.svg';
 
 const Navbar = class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       active: false,
-      navBarActiveClass: ""
+      navBarActiveClass: ''
     };
   }
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
+    const { active } = this.state;
     this.setState(
       {
-        active: !this.state.active
+        active
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
-        this.state.active
+        active
           ? this.setState({
-              navBarActiveClass: "is-active"
+              navBarActiveClass: 'is-active'
             })
           : this.setState({
-              navBarActiveClass: ""
+              navBarActiveClass: ''
             });
       }
     );
   };
 
   render() {
+    const { navBarActiveClass } = this.state;
     return (
       <nav
         className="navbar is-transparent"
@@ -42,11 +44,11 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+              className={`navbar-burger burger ${navBarActiveClass}`}
               data-target="navMenu"
               onClick={() => this.toggleHamburger()}
             >
@@ -55,10 +57,7 @@ const Navbar = class extends React.Component {
               <span />
             </div>
           </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
+          <div id="navMenu" className={`navbar-menu ${navBarActiveClass}`}>
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
                 About
