0e8221772f3d048a0e99d6451cf90b38792a180a
Zachary Townsend <zach@verbbrands.com>
2019-03-31 00:05:47 +0100

    Test project push

---
+++ src/templates/single-project.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleProject extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a single project</h1>
+      </div>
+    )
+  }
+}
