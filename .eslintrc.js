680a6d097e46adb5249846fc16a749b0a0c09189
Zachary Townsend <zach@verbbrands.com>
2019-03-30 23:07:52 +0100

    Adding global themes

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,12 @@ module.exports = {
   ],
   rules: {
     "no-unused-expressions": ["error", {"allowTernary": true}],
+    "comma-dangle": ["always-multiline"],
     "react/jsx-filename-extension": true,
     "prettier/prettier": [
       "error",
       {
         "singleQuote": true,
-
       }
     ]
   },
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 // import { graphql } from 'gatsby';
-
+import styled from 'styled-components';
 import Layout from '../components/Layout';
 // import Features from '../components/Features';
 // import BlogRoll from '../components/BlogRoll';
