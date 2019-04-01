75b9c0b07e0bc4f7247db4a67577780843e61985
Zachary Townsend <zach@verbbrands.com>
2019-04-01 14:29:55 +0200

    Setting up styles framework

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,15 +1,54 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+import { graphql, Link } from 'gatsby';
 import styled from 'styled-components';
 import Layout from '../components/Layout';
+import Button from '../components/Button';
 // import Features from '../components/Features';
 // import BlogRoll from '../components/BlogRoll';
 
-export const IndexPageTemplate = () => (
-  <div>
-    <h1>Soon this will be something cool</h1>
-  </div>
+const StyledPageContainer = styled.div`
+  background: url(${props => props.backgroundimage});
+  min-height: 100vh;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  text-align: center;
+  /* background: url('/assets/images/desktop-large_home-bg.jpg') no-repeat center center; */
+  background-size: cover;
+`;
+
+const StyledSiteTitle = styled.header`
+  text-align: center;
+
+  h1 {
+    font-size: 36px;
+    line-height: 1.2;
+    color: ${props => props.theme.white};
+  }
+
+  p.subtitle {
+    color: ${props => props.theme.lightGray};
+    font-size: 18px;
+    line-height: 1.1667;
+    margin-bottom: 27px;
+
+    &.highlight {
+      color: ${props => props.theme.primary};
+    }
+  }
+`;
+
+export const IndexPageTemplate = props => (
+  <StyledPageContainer className="section" backgroundimage={props.backgroundimage.childImageSharp.original.src}>
+    {props.title &&
+      <StyledSiteTitle>
+        <h1>{props.title}</h1>
+        {props.subtitle && <p className="subtitle">{props.subtitle}</p>}
+        <Button><Link to="/contact">Contact Me</Link></Button>
+      </StyledSiteTitle>
+    }
+  </StyledPageContainer>
 );
 
 IndexPageTemplate.propTypes = {
