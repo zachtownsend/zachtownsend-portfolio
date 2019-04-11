b0454a94a81d69176376b970e05982317990bf99
Zachary Townsend <zach@verbbrands.com>
2019-04-11 23:06:08 +0200

    Added slider

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,34 +1,54 @@
 import React from 'react';
-
+import { StaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+import Swiper from 'swiper';
 import Layout from '../../components/Layout';
 import StyledPageContainer from '../../styles/StyledPageContainer';
+import '../../../node_modules/swiper/dist/css/swiper.min.css';
 
-export default class ProjectIndexPage extends React.Component {
+const SwiperContainer = styled.div`
+  width: 100%;
+  height: 100%;
+
+  .swiper-slide {
+    width: auto;
+
+    .project-container {
+      img {
+        height: 75vh;
+        width: auto;
+      }
+    }
+  }
+`;
+
+class ProjectIndexPage extends React.Component {
+  componentDidMount = () => {
+    const mySwiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: '10%',
+    });
+  };
+
   render() {
+    const { data } = this.props;
+    const projects = data.allMarkdownRemark.edges.map(project => (
+      <div className="swiper-slide" key={project.node.id}>
+        <div className="project-container">
+          <h2>{project.node.frontmatter.title}</h2>
+          <img src={project.node.frontmatter.thumbnail.publicURL} alt="" />
+        </div>
+      </div>
+    ));
     return (
       <Layout>
-        <StyledPageContainer className="section">
-          <div className="container">
-            <div className="content">
-              <div
-                className="full-width-image-container margin-top-0"
-                style={{
-                  backgroundImage: `url('/img/blog-index.jpg')`,
-                }}
-              >
-                <h1
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem',
-                  }}
-                >
-                  Latest Stories
-                </h1>
-              </div>
-            </div>
+        <StyledPageContainer>
+          <div className="content">
+            <SwiperContainer className="swiper-container">
+              <div className="swiper-wrapper">{projects}</div>
+            </SwiperContainer>
           </div>
         </StyledPageContainer>
       </Layout>
@@ -36,23 +56,36 @@ export default class ProjectIndexPage extends React.Component {
   }
 }
 
-// query ProjectsArchive {
-//   allMarkdownRemark(
-//     limit: 10
-//     filter: { frontmatter: { templateKey: { eq: "single-project" } } }
-//   ) {
-//     edges {
-//       node {
-//         frontmatter {
-//           title
-//           thumbnail {
-//             id
-//           }
-//           client
-//           techs
-//         }
-//         rawMarkdownBody
-//       }
-//     }
-//   }
-// }
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query ProjectsArchive {
+        allMarkdownRemark(
+          limit: 10
+          filter: { frontmatter: { templateKey: { eq: "single-project" } } }
+        ) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                thumbnail {
+                  publicURL
+                }
+                client
+                techs
+              }
+              rawMarkdownBody
+            }
+          }
+        }
+        markdownRemark(frontmatter: { templateKey: { eq: "single-project" } }) {
+          frontmatter {
+            title
+          }
+        }
+      }
+    `}
+    render={projects => <ProjectIndexPage data={projects} {...props} />}
+  />
+);
