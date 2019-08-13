0c67cc9e523cb73581b0aa85cd3101f4152deea8
Zachary Townsend <zach@verbbrands.com>
2019-08-13 17:56:39 +0200

    fix(rebase): fixes after rebase

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
-import Helmet from 'react-helmet';
 import styled from 'styled-components';
 import Swiper from 'swiper';
 import posed, { PoseGroup } from 'react-pose';
